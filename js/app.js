3afab304fcecca8982554079b39c850db86e940d
kkas <kentakikui@gmail.com>
2015-11-04 17:02:01 +0900

    refactor: move venueModel into a new file

--- js/app.js
+++ js/app.js
@@ -13,16 +13,7 @@ myApp.main = function() {
 
   var CLIENT_ID = 'UIIR1040LGBT4NTI3IGTRUYQY5S5WTQ12CVTPP4YAQYGYAFC',
     //TODO: Replace this secret with something else!
-    CLIENT_SECRET = 'DRK32O210T1JHBMFU1GF3PGI3GVYZXUIFADJP4NFC1JVWJXJ',
-
-    /**
-     * Size of an icon on the map.
-     * According to the following document, the available sizes are 32, 44, 64,
-     * and 88.
-     * https://developer.foursquare.com/docs/responses/category
-     * @type {String}
-     */
-    ICON_SIZE = '32';
+    CLIENT_SECRET = 'DRK32O210T1JHBMFU1GF3PGI3GVYZXUIFADJP4NFC1JVWJXJ';
 
   /**
    * [initialLocation description]
@@ -41,99 +32,6 @@ myApp.main = function() {
   var WIKIPEDIA_API_BASE ='https://en.wikipedia.org//w/api.php' +
       '?action=query&prop=extracts&format=json&exintro=' +
       '&titles=San%20Francisco&callback=?';
-
-  /**
-   * Model that contains
-   * @param {[type]} data [description]
-   */
-  var VenueModel = function(data) {
-    var self = this,
-    categories = data.categories,
-    icon;
-
-    /**
-     * Creates a content that will be displayed in a
-     * infoWindow.
-     *
-     * @return {String} string that contains the HTML to be
-     * used in the infoWindow.
-     */
-    self.createInfoWindowContent = function() {
-      var infoWindowContentHTML = '<div class="content">',
-        keys = [
-          'name',
-          'contact',
-          'popular',
-          'likes',
-          'shortUrl'
-        ];
-
-      keys.forEach(function(key) {
-        infoWindowContentHTML += '<p>' + self[key] + '</p>';
-      });
-
-      infoWindowContentHTML += '</div>';
-
-      return infoWindowContentHTML;
-    };
-
-    /**
-     * Create a contact that will be displayed in a infoWindow.
-     * @param  {Object} contact  Object that is returned from the API and
-     * contains contact info
-     * @return {String} string that contains the HTML to be used in the
-     * infoWindow.
-     */
-    self.createContact = function(contact) {
-      var contactHTML = '<ul class="list-inline">',
-        key;
-
-      for (key in contact) {
-        if (contact[key]) {
-          contactHTML += '<li>' + '<strong>' + key + ':</strong>' +
-           contact[key] + '</li>';
-        }
-      }
-
-      contactHTML += '</ul>';
-
-      return contactHTML;
-    };
-
-    /*
-     * -----------------------
-     * Properties for venue
-     * -----------------------
-     */
-    self.name = data.name || '';
-    self.description = data.description || '';
-    self.contact = self.createContact(data.contact) || '';
-    self.popular = data.popular || '';
-    self.likes = data.likes || '';
-    self.shortUrl = data.shortUrl || '';
-
-    /*
-     * categories can be empty. See the doc for this for more info.
-     * https://developer.foursquare.com/docs/responses/venue
-     */
-    if (categories.length > 0) {
-      icon = categories[0].icon;
-      self.icon = icon.prefix + 'bg_' + ICON_SIZE + icon.suffix;
-    } else {
-      // Use the default google map icon if the categories array is empty.
-      self.icon = undefined;
-    }
-
-    /*
-     * -----------------------
-     * Properties for map
-     * -----------------------
-     */
-    self.position = new google.maps.LatLng(
-      data.location.lat, data.location.lng);
-    self.marker = undefined;
-    self.infoWindowContent = self.createInfoWindowContent(self);
-  }; // end of VenueModel
 
   /**
    * ViewModel of this application.
@@ -222,7 +120,7 @@ myApp.main = function() {
     self.addVenues = function(venuesAry) {
       venuesAry.forEach(function(data) {
         // Create new venue object
-        self.venueList.push(new VenueModel(data));
+        self.venueList.push(new myApp.VenueModel(data));
       });
     };
 
---
+++ js/venueModel.js
@@ -0,0 +1,107 @@
+// Global variable.
+var myApp = myApp || {};
+
+(function(app) {
+  'use strict';
+
+  /**
+   * Size of an icon on the map.
+   * According to the following document, the available sizes are 32, 44, 64,
+   * and 88.
+   * https://developer.foursquare.com/docs/responses/category
+   * @type {String}
+   */
+  var ICON_SIZE = '32';
+
+  /**
+   * Model that contains venue infomation
+   */
+  app.VenueModel = function(data) {
+    var self = this,
+    categories = data.categories,
+    icon;
+
+    /**
+     * Creates a content that will be displayed in a
+     * infoWindow.
+     *
+     * @return {String} string that contains the HTML to be
+     * used in the infoWindow.
+     */
+    self.createInfoWindowContent = function() {
+      var infoWindowContentHTML = '<div class="content">',
+        keys = [
+          'name',
+          'contact',
+          'popular',
+          'likes',
+          'shortUrl'
+        ];
+
+      keys.forEach(function(key) {
+        infoWindowContentHTML += '<p>' + self[key] + '</p>';
+      });
+
+      infoWindowContentHTML += '</div>';
+
+      return infoWindowContentHTML;
+    };
+
+    /**
+     * Create a contact that will be displayed in a infoWindow.
+     * @param  {Object} contact  Object that is returned from the API and
+     * contains contact info
+     * @return {String} string that contains the HTML to be used in the
+     * infoWindow.
+     */
+    self.createContact = function(contact) {
+      var contactHTML = '<ul class="list-inline">',
+        key;
+
+      for (key in contact) {
+        if (contact[key]) {
+          contactHTML += '<li>' + '<strong>' + key + ':</strong>' +
+           contact[key] + '</li>';
+        }
+      }
+
+      contactHTML += '</ul>';
+
+      return contactHTML;
+    };
+
+    /*
+     * -----------------------
+     * Properties for venue
+     * -----------------------
+     */
+    self.name = data.name || '';
+    self.description = data.description || '';
+    self.contact = self.createContact(data.contact) || '';
+    self.popular = data.popular || '';
+    self.likes = data.likes || '';
+    self.shortUrl = data.shortUrl || '';
+
+    /*
+     * categories can be empty. See the doc for this for more info.
+     * https://developer.foursquare.com/docs/responses/venue
+     */
+    if (categories.length > 0) {
+      icon = categories[0].icon;
+      self.icon = icon.prefix + 'bg_' + ICON_SIZE + icon.suffix;
+    } else {
+      // Use the default google map icon if the categories array is empty.
+      self.icon = undefined;
+    }
+
+    /*
+     * -----------------------
+     * Properties for map
+     * -----------------------
+     */
+    self.position = new google.maps.LatLng(
+      data.location.lat, data.location.lng);
+    self.marker = undefined;
+    self.infoWindowContent = self.createInfoWindowContent(self);
+  }; // end of VenueModel
+})(myApp);
